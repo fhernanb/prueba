3d7c88ac1a2fa6de68a78f32250285e223fa14d1
fhernanb <fhernanb@unal.edu.co>
2018-07-03 10:29:45 -0500

    prueba con este test

--- tests/testthat/test-loglik.R
+++ tests/testthat/test-loglik.R
@@ -2,8 +2,8 @@
 test_that("counts are as expected", {
   set.seed(1974)
   x <- rpois(n=50, lambda=4.5)
-
-  expect_equal(loglik(x, dist='dpois', param=list(lambda=4)), -106.4437)
+  rta <- round(loglik(x, dist='dpois', param=list(lambda=4)), 1)
+  expect_equal(rta, -106.4)
 
 })
 
