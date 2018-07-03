fcbfcbac450d8583478e7ab7d93db59293f62bb5
fhernanb <fhernanb@unal.edu.co>
2018-07-03 09:13:05 -0500

    prueba con una función :fire:

---
+++ R/loglik.R
@@ -0,0 +1,26 @@
+#' Log-likelihood
+#' 
+#' This function calculates the log-likelihood at some parameter values given the random sample and distribution.
+#' 
+#' @param x numeric vector.
+#' @param dist density name for the distribution in the form \code{dxxx}. By default it's value is \code{dnorm} but can be used any density.
+#' @param param list with the parameter values at which can be calculated the log-likelihood. 
+#' 
+#' @examples 
+#' # For Poisson distribution
+#' x <- rpois(n=50, lambda=4.5)
+#' loglik(x, dist='dpois', param=list(lambda=4))
+#' loglik(x, dist='dpois', param=list(lamb=4.5))
+#' loglik(x, dist='dpois', param=list(lambda=5))
+#' 
+#' # For normal distribution
+#' y <- rnorm(n=100, mean=170, sd=10)
+#' loglik(y, dist='dnorm', param=list(mean=170, sd=10))
+#' 
+#'
+#' @export
+#'
+loglik <- function(x, dist='dnorm', param=list(mean=0, sd=1)) {
+  ll <- do.call(dist, c(list(x=x), param, log=TRUE))
+  return(sum(ll))
+}
