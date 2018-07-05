8bca836e549bc8867c4db6dc8c29417248ceb206
fhernanb <fhernanb@unal.edu.co>
2018-07-04 20:47:22 -0500

    se mejoro description

--- R/fun.R
+++ R/fun.R
@@ -1,5 +1,7 @@
 #' Add together two numbers.
 #' 
+#' Esta funcion sirve para obtener la suma de dos numeros reales.
+#' 
 #' @param x A number.
 #' @param y A number.
 #' @return The sum of \code{x} and \code{y}.
