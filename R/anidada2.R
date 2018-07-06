f76cee863ee25700668d7f5c39e11ae342c14918
fhernanb <fhernanb@unal.edu.co>
2018-07-06 09:29:31 -0500

    se agregó otra anidada

---
+++ R/anidada2.R
@@ -0,0 +1,20 @@
+#' multi2
+#' 
+#' Esta funcion sirve para obtener la multiplicacion de dos numeros reales.
+#' 
+#' @param x A number.
+#' @param y A number.
+#' @return The sum of \code{x} and \code{y}.
+#' @examples
+#' multi2(1, 1)
+#' multi2(10, 1)
+#' @export
+multi2 <- function(x, y) {
+  res <- myaux2(x, y)
+  res
+}
+#' @export
+myaux2 <- function(x, y) {
+  x * y
+}
+
