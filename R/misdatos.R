5674319aa233972156f4ae840a1dde89126884fc
fhernanb <fhernanb@unal.edu.co>
2018-07-09 18:03:04 -0500

    Se creo este ejemplo para Deisy :sweat_drops:

--- R/misdatos.R
+++ R/misdatos.R
@@ -2,7 +2,7 @@
 #'
 #' Estos son uno datos de prueba para mostrarle a Deisy como se crean.
 #' 
-#' @format A data frame with 3 rows and 2 variables:
+#' @format This data frame with 3 rows and 2 variables:
 #' \describe{
 #'   \item{estatura}{altura de las personas en centimetros}
 #'   \item{peso}{peso corporal de las personas en kilogramos}
