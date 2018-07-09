99ddcd6074ff4e466b74476bfab570b81800c8aa
fhernanb <fhernanb@unal.edu.co>
2018-07-09 16:41:54 -0500

    se agrego un conjunto de datos como ejemplo

---
+++ R/misdatos.R
@@ -0,0 +1,12 @@
+#' Mis datos de prueba
+#'
+#' Estos son uno datos de prueba para mostrarle a Deisy como se crean.
+#' 
+#' @param estatura altura de las personas en centimetros.
+#' @param peso peso corporal de las personas en kilogramos.
+#'
+#' @format Time series data
+#' @source Australian Bureau of Statistics.
+#' @keywords datasets
+#'
+"misdatos"
