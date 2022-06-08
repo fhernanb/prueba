0c8de3f373ca3dc5316d3a8bb60d28cf460dbe23
fhernanb <fhernanb@unal.edu.co>
2022-06-08 15:51:11 -0500

    cambios menores

--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(prueba)
-
-test_check("prueba")
--- tests/testthat/test-loglik.R
+++
@@ -1,9 +0,0 @@
-
-test_that("prueba con loglik", {
-  set.seed(1974)
-  x <- rpois(n=50, lambda=4.5)
-  rta <- round(loglik(x, dist='dpois', param=list(lambda=4)), 1)
-  expect_equal(rta, -106.4)
-
-})
-
